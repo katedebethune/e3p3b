a9c924f2bd1edb9aceb5aa34215a090008841d86
Kate deBethune <kdebethune@gmail.com>
2015-04-20 23:01:50 -0400

    final changes to js before submission

--- js/hw3b.js
+++ js/hw3b.js
@@ -1,10 +1,20 @@
 /* hw3b.js */
 
-//window.onload = problem3;
+/* 
+ * P3B - Kate de Bethune - April 20, 2015
+ *
+ * I have implemented solutions to problems 1 through 4. 
+ * 
+ * My solution to problem 3 borrows from Rob Frenette's & Larry's examples.
+ * Other attributions are given inline.
+ *
+ */
+
 window.addEventListener("load", problem1);
 window.addEventListener("load", problem2);
 window.addEventListener("load", problem3);
 window.addEventListener("load", problem4);
+
 
 /************ START PROBLEM 1 ************************/
 function problem1 () {
@@ -12,16 +22,16 @@ function problem1 () {
 	var password1 = document.getElementById("pwd1");
 	var password2 = document.getElementById("pwd2");
 	var p1_value = null;
-	var p2_value = null;
+	
+	// Page starts by hiding the pwdMatch message "Passwords Match!"
 	document.getElementById("pwdMatch").style.display = "none";
 
+	// password1 validation
 	password1.addEventListener("focus", function() {
 		var min_char = 8;
-	
-		if ( !this.value || this.value.length < min_char ) {
-			document.getElementById("pwd1Hint").style.display = "inline";
-		}
-	
+		
+		// Use keyup event listener to count chars in the password field.
+		// Remove warning message once 8 chars have been typed.
 		this.addEventListener("keyup", function () {
 			if ( this.value.length < min_char ) {
 				document.getElementById("pwd1Hint").style.display = "inline";
@@ -30,23 +40,31 @@ function problem1 () {
 				document.getElementById("pwd1Hint").style.display = "none";
 			}
 		});
-	
+		
+		// assign this.value to p1_value for use in password2 comparison.
 		this.addEventListener("blur", function() {
 			console.log(this.value);
 			p1_value = this.value;
 		});
 	});
 
+	// password2 validation
 	password2.addEventListener("focus", function() {
+		// If a value is associated with password2 and the text box
+		// receives focus again, set its value to null.
 		if ( this.value ) {
-			this.value = "";
+			this.value = null;
 			document.getElementById("pwdMatch").style.display = "none";
 		}
 	
+		// Assumes the this.value for password2 == null and p1_value has been
+		// instantiated.
 		if ( p1_value ) {
 			document.getElementById("pwd2Hint").style.display = "inline";
 		}
 	
+		// password2 keyup function watches for a match on p1_value:
+		// Displays "no match" message until / unless the two strings match.
 		this.addEventListener("keyup", function () {
 			if ( this.value != p1_value ) {
 				document.getElementById("pwd2Hint").style.display = "inline";
@@ -54,13 +72,15 @@ function problem1 () {
 			else {
 				document.getElementById("pwd2Hint").style.display = "none";
 			}
-			//console.log(this.value);
 		});
 	
+		// On password2 blur, a "passwords match" message is displayed if the two 
+		// strings match. Otherwise, the password2.value is reset to null and the focus
+		// is set to the password2 text box.
 		this.addEventListener("blur", function() {
 			if ( this.value != p1_value ) {
 				document.getElementById("pwd2Hint").style.display = "inline";
-				password2.value = "";
+				password2.value = null;
 				password2.focus();
 			}
 			else {
@@ -75,8 +95,6 @@ function problem1 () {
 }
 /************ END PROBLEM 1 ************************/
 
-
-
 /************ START PROBLEM 2 ************************/
 function problem2 () {
 	'use strict';
@@ -87,7 +105,9 @@ function problem2 () {
 		if ( this.value.length <= char_limit ) {
 			document.getElementById("charsLeft").innerHTML = (char_limit - this.value.length);
 		}
-		//the idea for this block came from: http://stackoverflow.com/questions/10414420/how-to-prevent-user-to-enter-text-in-textarea-after-reaching-max-character-limit
+		// the idea for this block came from: 
+		// http://stackoverflow.com/questions/10414420/how-to-prevent-user-to-enter-text-in-textarea-after-reaching-max-character-limit
+		// the input box prevents overflow characters.
 		else {
 			this.value = this.value.substring(0, char_limit);
 		}
@@ -103,6 +123,7 @@ function problem3 () {
 
 	var subSelectField = document.getElementById("secondSelect"); 
 	
+	// Flag variable for use in finite state machine logic
 	var subSelectSet = 'FALSE';
 
 	//create an array with the options for the select field
@@ -142,42 +163,25 @@ function problem3 () {
 	// 1. call the second function to build up the second select box
 	// 2. pass the original select through to the second function
 	selectField.addEventListener("change", function(){
-	//selectField.onchange = function(){
 		var myVal = selectField.value;
 	
-	
-		//console.log(myVal);
-		//console.log(selectField.value.length);
-		//console.log(myObj[selectField.value]);
-		//console.log(myObj[selectField.value].length);
-		//console.log(myObj[selectField.value][0]);
-		//alert("optionsSubselectLength is " + optionsSubSelect.length);
-		//clear out subSelect if there are any values in there
-		
+		// a little bit of finite state machine logic here to clear the subSelect fields
+		// when a new main selection is chosen.		
 		if ( subSelectSet == 'TRUE' ) {
 			// http://www.2ality.com/2012/12/clear-array.html
 			optionsSubSelect = [];
 			// http://www.somacon.com/p542.php
 			subSelectField.options.length = 0;
+			// subselect has been cleared, set flag to false.
 			subSelectSet == 'FALSE';
 		}
-		//alert("optionsSubselectLength is " + optionsSubSelect.length);
-		//alert("subSelectField.options.length is " + subSelectField.options.length);
 		
-		
+		// Populate optionsSubSelect array for current main select.
 		for(var j = 0 ; j < myObj[selectField.value].length; j++) {
-			console.log(myObj[selectField.value][j]);
 			optionsSubSelect.push(myObj[selectField.value][j]);
 		} 
-		//alert("this is optionsSubSelect " + optionsSubSelect);
-	
-		//subSelectField.addEventListener("change", function(){
-		//subSelectField.onchange = function() {
-			//alert("subSelectField fired");
-		
-		// Want this to run all the time, we just want to removeChild if
-		// a set of menu options is already in there.
 		 
+		// Create DOM elements from optionsSubSelect array
 		for ( var k = 0 ; k < optionsSubSelect.length ; k++ ) {
 
 			//create an option tag
@@ -185,31 +189,20 @@ function problem3 () {
 
 			//add the strings from your array into each option tag
 			var words2 = document.createTextNode(optionsSubSelect[k]);
-			//console.log("this is words2 " + optionsSubSelect[k]);
 
 			//set the value of each option to the strings from your array
 			element2.value = optionsSubSelect[k];
-			//console.log("this is element2.value " + element2.value);
 
 			//append each Text Node to each option tag.
 			element2.appendChild(words2);
 
-			// set value="" on the <option>
-			element2.setAttribute("value", words2);
-
 			//append each option tag to to the select field
 			subSelectField.appendChild(element2);
 			
-			//set flag
+			//set finite state machine flag to indicate subselect is populated.
 			subSelectSet = 'TRUE';
 		}
 	});
-	
-	/*
-	subSelectField.addEventListener("change", function(){
-		alert("subSelectField triggered.");
-	}); */
-
 }  
 /************ END PROBLEM 3 ************************/
 
@@ -219,16 +212,18 @@ function problem4() {
 	document.getElementById("secondInstrumentDetail").style.visibility = "hidden";
 	// Solution adapted from this SO post: 
 	// http://stackoverflow.com/questions/8922002/attach-event-listener-through-javascript-to-radio-button
-		var x = document.getElementsByName("secondInstRadio");
-		for(var i = 0 ; i < x.length ; i ++) {
-			x[i].onclick = function() {
-				if ( this.value == "No" ) {
-					document.getElementById("secondInstrumentDetail").style.visibility = "hidden";
-				}
-				else {
-					document.getElementById("secondInstrumentDetail").style.visibility = "visible";
-				}
+	var x = document.getElementsByName("secondInstRadio");
+	for(var i = 0 ; i < x.length ; i ++) {
+		x[i].onclick = function() {
+			if ( this.value == "No" ) {
+				document.getElementById("secondInstrumentDetail").style.visibility = "hidden";
+			}
+			else {
+				document.getElementById("secondInstrumentDetail").style.visibility = "visible";
 			}
 		}
+	}
 }
 /************ END PROBLEM 4 ************************/
+
+
